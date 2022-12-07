2576ceaf43f663b35ec68d55c6090ba99c076eca
BR0KENz <46622642+BR0KENz@users.noreply.github.com>
2022-12-07 19:03:07 +0700

    Update log.lua

--- log.lua
+++ log.lua
@@ -85,7 +85,7 @@ end)
 
 -- print("Anti AFK Is Loaded")
 Nexus_Version = 101
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RBX%20Alt%20Manager/Nexus/Nexus.lua"))()
+loadstring(game:HttpGet"https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RBX%20Alt%20Manager/Nexus/Nexus.lua")()
 task.spawn(function()
     Nexus:Connect()
 end)
